0e550bbcd2a466b98167d945b82e01e1f836a94a
jalbersrp <alberto.sanchez@particle.io>
2023-09-04 11:20:00 -0600

    Initial commit

---
+++ target/5.5.0-rc.1/p2/obj/src/module_info.o.d
@@ -0,0 +1,39 @@
+/Users/alberto/Documents/Particle_Workbench/AssetOTA/UnoAssetOta/target/5.5.0-rc.1/p2//obj/./src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h \
+ ../../../hal/inc/hal_platform.h ../../../hal/shared/platforms.h \
+ ../../../hal/src/tron/hal_platform_config.h \
+ ../../../hal/src/rtl872x/hal_platform_rtl8721x_config.h
+../../../dynalib/inc/module_info.inc:
+../../../dynalib/inc/module_info.h:
+../../../services/inc/static_assert.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h:
+/Users/alberto/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h:
+../../../hal/inc/hal_platform.h:
+../../../hal/shared/platforms.h:
+../../../hal/src/tron/hal_platform_config.h:
+../../../hal/src/rtl872x/hal_platform_rtl8721x_config.h:
---
+++ target/5.5.0-rc.1/p2/obj/src/user_module.o.d
@@ -0,0 +1,7 @@
+/Users/alberto/Documents/Particle_Workbench/AssetOTA/UnoAssetOta/target/5.5.0-rc.1/p2//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
+../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
+../../../hal/shared/platforms.h:
